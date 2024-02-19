da9d1884c334ab4a44c9e27709a137c2064cc382
suxue-mac <suxuewb@gmail.com>
2024-02-19 16:30:08 +0800

    info

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -18,7 +18,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     locales: {
         '/': {
             lang: 'zh-CN',
-            title: "Evan's blog",
+            title: "宿雪冷音",
             description: 'web前端技术博客,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,React,python,css3,html5,Node,git,github等技术文章。',
         }
     },
@@ -64,7 +64,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         ],
         sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
         logo: '/img/logo.png', // 导航栏logo
-        repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+        repo: 'suxuelengyin/suxuewb-vuepress-blog', // 导航栏右侧生成Github链接
         searchMaxSuggestions: 10, // 搜索结果显示最大数
         lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
         docsDir: 'docs', // 编辑的文件夹
@@ -118,7 +118,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         // 博主信息 (显示在首页侧边栏)
         blogger: {
             avatar: 'https://jsd.cdn.zzko.cn/gh/xugaoyi/image_store/blog/20200103123203.jpg',
-            name: 'Evan Xu',
+            name: '宿雪冷音',
             slogan: '前端界的小学生',
         },
 

233f2c9688fc579ebe169874db1b32f3a169bba9
suxue-mac <suxuewb@gmail.com>
2024-03-01 16:55:14 +0800

    AI

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -63,6 +63,15 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
                         link: '/pages/48023d/'
                     },
                 ]
+            },
+            {
+                text: 'AI', link: '/AI/',
+                items: [
+                    {
+                        text: 'Copilot',
+                        link: '/pages/e0e977/'
+                    },
+                ]
             }
         ],
         sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
---
+++ docs/00.目录页/AI.md
@@ -0,0 +1,18 @@
+---
+pageComponent:
+  name: Catalogue
+  data:
+    path: 04.AI
+    imgUrl: /img/web.png
+    description: AI
+title: AI
+date: 2020-03-11 21:50:53
+permalink: /AI/
+sidebar: false
+article: false
+comment: false
+editLink: false
+author:
+  name: suxuewb
+  link: https://github.com/suxuelengyin
+---
